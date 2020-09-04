2a766929c65fd62e45e3cea2cdded80b2eae4b9b
Unitarou <atelierkisame@gmail.com>
2020-09-04 13:11:05 +0900

    チューの音追加したりした！

--- src/main/kotlin/com/github/namiuni/moripakiss/KissListener.kt
+++ src/main/kotlin/com/github/namiuni/moripakiss/KissListener.kt
@@ -2,12 +2,13 @@ package com.github.namiuni.moripakiss
 
 import org.bukkit.Location
 import org.bukkit.Particle
-import org.bukkit.entity.Entity
-import org.bukkit.entity.EntityType
+import org.bukkit.Sound
+import org.bukkit.SoundCategory
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEntityEvent
+import org.bukkit.inventory.EquipmentSlot
 import java.util.*
 
 
@@ -15,21 +16,24 @@ class KissListener :Listener  {
 
     @EventHandler
     fun onClickedPlayer(event: PlayerInteractEntityEvent) {
-        if (event.rightClicked !is Player) return
-        if (!event.player.isSneaking) return
+        if (event.rightClicked !is Player ) return
+        if (event.hand !== EquipmentSlot.HAND) return
 
         val player = event.player
         val clickedPlayer = event.rightClicked as Player
 
+        if (!player.isSneaking || !clickedPlayer.isSneaking) return
+
         val playerEyeLocation = player.eyeLocation
-        val clickedPlayerFaceLocation = clickedPlayer.location.clone().add(0.0,1.75,0.0)
+        val clickedPlayerFaceLocation = clickedPlayer.eyeLocation
 
         spawnParticles(playerEyeLocation, clickedPlayerFaceLocation)
     }
 
     private fun spawnParticles(loc1: Location, loc2: Location) {
         val kissLoc: Location = loc1.clone().add((loc2.x - loc1.x) / 2, (loc2.y - loc1.y) / 2, (loc2.z - loc1.z) / 2)
-        val amount: Int = 5 + Random().nextInt(10 - 5 + 1)
+        val amount: Int = 3 + Random().nextInt(3)
         loc1.world.spawnParticle(Particle.HEART, kissLoc, amount, 0.3, 0.3, 0.3)
+        loc1.world.playSound(kissLoc,Sound.ENTITY_BAT_AMBIENT,SoundCategory.MASTER,1.0f,1.2f)
     }
 }
